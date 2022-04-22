913f2d51a7440db3987bc5dd25d7e9fc14e25ce8
dorlevi28 <73902503+dorlevi28@users.noreply.github.com>
2022-04-22 12:28:21 +0300

    dor changes
    added directory choice instead of file (since zssr takes directory as input)
    managed to assemble the command for zssr train 
    removed unnecessary buttons 
    Now need to run in in Pycharm using BGU GPU and see how to reattach the output to the viewer

--- napari-zssr/napari_zssr/zssr/zssr_gui.py
+++ napari-zssr/napari_zssr/zssr/zssr_gui.py
@@ -1,6 +1,6 @@
+import os
 from typing import Any
 from napari_plugin_engine import napari_hook_implementation
-
 import time
 import numpy as np
 
@@ -24,59 +24,70 @@ def read_logging(log_file, logwindow):
 
 # logo = os.path.join(__file__, 'logo/logo_small.png')
 
+
+
 def widget_wrapper():
     from napari.qt.threading import thread_worker
     @magicgui(
         call_button='run "Zero-Shot" Super-Resolution ',
         layout='vertical',
-        label_line_1=dict(widget_type='Label', name='      '),
-        label_line_2=dict(widget_type='Label', name='Zero Shot Super Resolution for microscopic images '),
-        label_line_3=dict(widget_type='Label', name='       Ben Gurion University of the Negev'),
+        label_line_2=dict(widget_type='Label', label='            Zero Shot Super Resolution for microscopic images'),
+        label_line_3=dict(widget_type='Label', label='            Ben Gurion University of the Negev'),
         model_type=dict(widget_type='ComboBox', label='model type', choices=['zssr', ''],
                         value='zssr',
                         tooltip='there is a <em>cyto</em> model, a new <em>cyto2</em> model from user submissions, and a <em>nuclei</em> model'),
         custom_model=dict(widget_type='FileEdit', label='select 2D image: ',
-                          tooltip='specify file path to it here'),
-        flag_label=dict(widget_type='Label', name='      Set ZSSR configurations '),
-        zssr_flag_X4=dict(widget_type='CheckBox', text=' Estimate the X4 kernel', value=True),
-        zssr_flag_SR=dict(widget_type='CheckBox', text='Perform ZSSR using the estimated kernel', value=True),
-        zssr_flag_REAL=dict(widget_type='CheckBox', text='Real-image configuration (effects only the ZSSR)', value=False),
-        set_zssr_flag=dict(widget_type='PushButton', text='set ZSSR flag',
-                                  enabled=True),
+                          tooltip='specify file path to it here', mode='d'),
+        flag_label=dict(widget_type='Label', label='              Set ZSSR configurations '),
+        zssr_flag_X4=dict(widget_type='CheckBox', text=' Estimate the X4 kernel'),
+        zssr_flag_SR=dict(widget_type='CheckBox', text='Perform ZSSR using the estimated kernel'),
+        zssr_flag_REAL=dict(widget_type='CheckBox', text='Real-image configuration (effects only the ZSSR)'),
     )
+
     def widget(  # label_logo,
-            label_line_1,
             label_line_2,
             label_line_3,
             viewer: Viewer,
             image_layer: Image,
-
             model_type,
             custom_model,
             flag_label,
             zssr_flag_X4,
             zssr_flag_SR,
-            zssr_flag_REAL,
-            set_zssr_flag
+            zssr_flag_REAL
     ) -> None:
         # Import when users activate plugin
-        if not hasattr(widget, 'cellpose_layers'):
-            widget.cellpose_layers = []
-       # print(widget.zssr_flag_REAL.value)
-        image = image_layer.data
-        # put channels last
-        widget.n_channels = 0
-        widget.channel_axis = None
-        if image_layer.ndim == 4 and not image_layer.rgb:
-            chan = np.nonzero([a == 'c' for a in viewer.dims.axis_labels])[0]
-            if len(chan) > 0:
-                chan = chan[0]
-                widget.channel_axis = chan
-                widget.n_channels = image.shape[chan]
-        elif image_layer.ndim == 3 and not image_layer.rgb:
-            image = image[:, :, :, np.newaxis]
-        elif image_layer.rgb:
-            widget.channel_axis = -1
+        #if not hasattr(widget, 'cellpose_layers'):
+        #    widget.cellpose_layers = []
+       ## print(widget.zssr_flag_REAL.value)
+        #image = image_layer.data
+        ## put channels last
+        #widget.n_channels = 0
+        #widget.channel_axis = None
+        #if image_layer.ndim == 4 and not image_layer.rgb:
+        #    chan = np.nonzero([a == 'c' for a in viewer.dims.axis_labels])[0]
+        #    if len(chan) > 0:
+        #        chan = chan[0]
+        #        widget.channel_axis = chan
+        #        widget.n_channels = image.shape[chan]
+        #elif image_layer.ndim == 3 and not image_layer.rgb:
+        #    image = image[:, :, :, np.newaxis]
+        #elif image_layer.rgb:
+        #    widget.channel_axis = -1
+        zssr_run_path = "C:\\Users\\dorle\\PycharmProjects\\final-project-ZSSR-2.0\\ZSSR\KernelGAN-master\\train.py "
+        command = "python " + zssr_run_path +"--input-dir "
+        if custom_model: #if path given - run ZSSR with chosen flags
+            command += str(custom_model) + " "
+            if zssr_flag_SR:
+                command +="--SR "
+            if zssr_flag_REAL:
+                command +="--real "
+            if zssr_flag_X4:
+                command +="--x4"
+            print(command)
+            #os.system(command)
+
+
 
     return widget
 
